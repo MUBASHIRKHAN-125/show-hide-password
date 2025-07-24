58dbe08f4865736fd074acbb34a89d8e759bb780
MUBASHIRKHAN-PROGRAMMER <mk765pc@gmail.com>
2025-07-24 18:57:51 +0500

    code-fix

--- password.ts
+++ password.ts
@@ -10,10 +10,5 @@ function password() {
         userPassword.type = "text";
         imgUser.src = "image/img2.png"
         imgUser.style.top = "2px"
-    } else {
-        imgUser.src = "image/img1.png"
-        userPassword.type = "password";
-        imgUser.style.top = "6px"
     }
-
 }
